8e4bc03ef60a3636343861464ca4a979a0f56b91
kaynanoliveira <kaynanoliveira357@gmail.com>
2024-10-29 02:31:35 -0300

    alteração de nome da const

---
+++ src/prompts-schema/prompt-schema-qrcode.js
@@ -0,0 +1,20 @@
+import chalk from "chalk";
+
+const PromptSchemaQRCode = [
+    {
+        name: "link",
+        description: chalk.yellow.bold("Digite o link para gerar o QRCode"),
+    },
+    {
+        name: "type",
+        description: chalk.yellow.bold(
+            "Escolha o tipo de QRCode (1 - Normal ou 2 - Terminal)"
+        ),
+        pattern: /^[1-2]+$/,
+        message: chalk.red.italic("Escolha apenas entre 1 e 2"),
+        required: true,
+    }
+
+]
+
+export default PromptSchemaQRCode
